73276c4b5dadf2f669db9e6e4a296071bd1d7867
Marcin Lipa <marcin.lipa@protonmail.com>
2020-05-02 15:47:01 +0200

    3a

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello world!</h1>"
+    return render_template("index.html")
 
 
 @app.route("/user/<name>")
 def user(name: str):
-    return f"<h1>Hello {name}!</h1>"
+    return render_template("user.html", name=name)
