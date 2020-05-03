9b6ce03d4f35459624fd711acd740736e45a7b48
Marcin Lipa <marcin.lipa@protonmail.com>
2020-05-03 19:14:55 +0200

    5a

--- hello.py
+++ hello.py
@@ -1,23 +1,50 @@
-from datetime import datetime
+import os.path
 
 from flask import Flask, flash, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
+db_file_abspath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data.sqlite")
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "44zgw&8v(M*L92{/,22)F"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_file_abspath}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role")
+
+    def __repr__(self):
+        return f"<Role {self.name}>"
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
 class NameForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
-
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-
-app.config["SECRET_KEY"] = "44zgw&8v(M*L92{/,22)F"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -29,14 +56,7 @@ def index():
             flash("Looks like you've changed your name!")
         session["name"] = form.name.data
         return redirect(url_for("index"))
-    return render_template(
-        "index.html", current_time=datetime.utcnow(), name=session.get("name"), form=form
-    )
-
-
-@app.route("/user/<name>")
-def user(name: str):
-    return render_template("user.html", name=name)
+    return render_template("index.html", form=form, name=session.get("name"))
 
 
 @app.errorhandler(404)
