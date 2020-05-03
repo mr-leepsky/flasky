7c113554ae966e9179ce93f0148b7985f5f1adec
Marcin Lipa <marcin.lipa@protonmail.com>
2020-05-03 15:16:43 +0200

    4a

--- hello.py
+++ hello.py
@@ -3,15 +3,33 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config["SECRET_KEY"] = "44zgw&8v(M*L92{/,22)F"
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template(
+        "index.html", current_time=datetime.utcnow(), name=name, form=form
+    )
 
 
 @app.route("/user/<name>")
