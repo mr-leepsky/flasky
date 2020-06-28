d0f176718e754da53bab862b2f93229b6d2b3b14
Marcin Lipa <marcin.lipa@protonmail.com>
2020-06-28 20:26:05 +0200

    6a

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+import os
 import os.path
 
 from flask import Flask, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -17,11 +19,20 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "44zgw&8v(M*L92{/,22)F"
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_file_abspath}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["FLASK_MAIL_SUBJECT_PREFIX"] = "[Flasky] "
+app.config["FLASK_MAIL_SENDER"] = "Flasky Admin <admin@flasky.com>"
+app.config["FLASKY_ADMIN"] = os.environ.get("FLASKY_ADMIN")
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -49,6 +60,17 @@ class NameForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(
+        app.config["FLASK_MAIL_SUBJECT_PREFIX"] + subject,
+        sender=app.config["FLASK_MAIL_SENDER"],
+        recipients=[to],
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
@@ -60,6 +82,13 @@ def index():
             db.session.add(user)
             db.session.commit()
             session["known"] = False
+            if app.config["FLASKY_ADMIN"]:
+                send_mail(
+                    to=app.config["FLASKY_ADMIN"],
+                    subject="New User",
+                    template="mail/new_user",
+                    user=user,
+                )
         session["name"] = form.name.data
         return redirect(url_for("index"))
     return render_template(
@@ -74,7 +103,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template("501.html"), 500
+    return render_template("500.html"), 500
 
 
 @app.shell_context_processor
